3ed557d00d4aebddd88b0d7c64bac68da4cc3d80
Jespesa <jespesa@metropolia.fi>
2024-01-18 09:42:42 +0200

    xd

---
+++ Mod-01/teht1.py
@@ -0,0 +1,4 @@
+# Muuttujat ja vuorovaikutteiset ohjelmat
+Nimi = input("Kirjoita nimesi")
+print ("Mikä sinun nimesi on ?")
+print ("Terve",Nimi)
---
+++ Mod-01/tuntitehtava1.py
@@ -0,0 +1,5 @@
+#tässä on minun eka python-ohjelma.
+print
+print("Moi, minun nimeni on Jesper")
+print("Tässä on toka rivi")
+print("Tässä on kolmas rivi")
