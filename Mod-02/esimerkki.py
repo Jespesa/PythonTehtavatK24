776808a9feaa0461c1e64aecce6411c3748c3c7e
Jespesa <156893581+Jespesa@users.noreply.github.com>
2024-01-25 13:03:55 +0200

    Delete Mod-02/esimerkki.py

--- Mod-02/esimerkki.py
+++
@@ -1,9 +0,0 @@
-#Ehtolause (If)
-#kysytään käyttäjän ikä
-#muutetaan käyttäjän antama arvo heti kokonaisluvuksi
-ika = int(input("Anna ikäsi: "))
-if ika >=18:
-    print ("Olet siis täysi-ikäinen")
-    print ("Lähdetkö kahville ?")
-elif ika <18:print ("MENE NUKKUMAAN")
-print ("Ohjelma loppui")
